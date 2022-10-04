88d91288459018e4e1e7f14961f567831c1bd0f7
JoaoGMagri <jgmagri@hotmail.com>
2022-10-04 15:36:36 -0300

    Inicio do projeto

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import GlobalStyle from "./GlobalStyle";
+import Head from "./Head";
+
+export default function App() {
+
+    return (
+      <>
+        <GlobalStyle />
+        <Head />
+      </>
+    );
+  }
---
+++ src/components/Head.js
@@ -0,0 +1,23 @@
+//Funcionalidades
+import styled from "styled-components";
+
+export default function Head() {
+
+    return(
+
+        <ContainerHead>
+            <h1>CINEFLEX</h1>
+        </ContainerHead>
+
+    )
+
+}
+
+const ContainerHead = styled.header`
+
+    background-color: black;
+
+    h1{
+        color: #E8833A;
+    }
+`
